eaf1c297882383ae0f1c66c2a6149f1dd71b8fdb
yunhua.xiao <yunhua.xiao@dianping.com>
2014-09-30 00:24:08 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,48 @@
+var gulp = require('gulp');
 var less = require('gulp-less');
 var path = require('path');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+var del = require('del');
 
-gulp.task('less', function() {
-  gulp.src('./component/**/*.less')
-    .pipe(less({
-          paths: [path.join(__dirname, 'common/base'), path.join(__dirname, 'common/component')]
-        }))
-    .pipe(gulp.dest(function (file) {
-          return file.base.replace(file.cwd, '.');
-        }));
-
-  gulp.src('./common/img/**/*.*')
-    .pipe(gulp.dest('./component/img/'));
+gulp.task('clean', function (cb) {
+  del(['dist'], cb);
 });
 
-gulp.task('default', ['less']);
+gulp.task('less', function() {
+  gulp.src('./less/page/**/*.less')
+    .pipe(less({
+      paths: [path.join(__dirname, 'less', 'base'), path.join(__dirname, 'less', 'component')]
+    }))
+    .pipe(gulp.dest('dist/css/'));
+
+  gulp.src('./less/common/img/**/*.*')
+    .pipe(gulp.dest('dist/img/'));
+});
+
+gulp.task('html', ['clean'], function () {
+  return gulp.src('./view/**/*.html')
+    .pipe(gulp.dest('dist/view/'));
+});
+
+gulp.task('script', ['clean'], function () {
+  return gulp.src('./js/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist/js/'));
+});
+
+gulp.task('image', ['clean'], function() {
+  return gulp.src(['./less/common/img/**/*', './less/page/img/**/*'])
+    .pipe(imagemin({optimizationLevel: 5}))
+    .pipe(gulp.dest('dist/img/'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['./less/**/*'], ['less']);
+  gulp.watch(['./js/**/*'], ['script']);
+});
+
+gulp.task('default', ['less', 'image', 'script', 'html']);
